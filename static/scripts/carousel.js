681aca099939b00e6c7b8711b117118b7ba2326c
Napta Hag <naptahag@gmail.com>
2022-09-28 18:56:51 +0100

    said

--- static/scripts/carousel.js
+++ static/scripts/carousel.js
@@ -28,6 +28,9 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
+
+
 // Activate Carousel
 $("#myCarousel").carousel();
 
@@ -70,31 +73,58 @@ $(document).ready(function(){
 
 //carousel-p2
 
-var slidePosition = 1;
-SlideShow(slidePosition);
 
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow(slidePosition += n);
-}
+// Activate Carousel
+$("#myCarousel2").carousel();
 
-//  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
-}
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel2").carousel(1);
+});
 
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
-  }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
-} 
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#myCarousel2").carousel("prev");
+});
+
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel2").carousel({interval: 500, wrap: false});
+    
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel2").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel2").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel2").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel2").carousel(3);
+  });
+    
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel2").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel2").carousel("next");
+  });
+});
+
+
+// Activate Carousel
+$("#myCarousel2").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel2").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#myCarousel2").carousel("prev");
+});
+
