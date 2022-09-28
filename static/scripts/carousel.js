f37db0279b5eb2ebd0a2cdacf80b38e6175847fa
Napta Hag <naptahag@gmail.com>
2022-09-28 16:55:51 +0100

    carousel

--- static/scripts/carousel.js
+++ static/scripts/carousel.js
@@ -1,45 +1,4 @@
 
-
-
-
-//const track =document.querySelector('.carousel_track');
-//const slides= Array.from(track.children);
-//const nextButton = document.querySelector('.carousel_button--right');
-//const prevButton = document.querySelector('.carousel_button--left');
-//onst dotsNav = document.querySelector ('.carousel_nav');
-//const dots = Array.from(dotsNav.children);
-
-//const slideWidth = slides[0].getBoundingClientRect().width;
-
-//console.log(slideWidth);
-
-//arrange slides next to one another
-//slides[0].style.left = 0;
-//slides[1].style.left = '200px';
-//slides[0].style.left = 0;
-//slides[0].style.left = 0;
-
-//function setSlidePosition(slide, index) {
-    //slide.style.left = slideWidth * index + 'px';
-
-//slides.forEach = (setSlidePosition);
-
-
-//when I click left, move slide to the left
-
-
-//When I click right, move slide to the right
-//nextButton.addEventListener('click', e=> {
-    //const currentSlide = track.querySelector('.current-slide');
-    //const nextSlide = currentSlide.nextElementSibling;
-    //const amountToMove= nextSlide.style.left;
-    //move to the next slide
-    //track.style.tranform = 'translateX(-635.2px' + amountToMove +')';
-
-
-//When I click nav indicators; move to that slide
-
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -68,3 +27,74 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#myCarousel").carousel("prev");
+});
+
+$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel({interval: 500, wrap: false});
+    
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
+    
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
+
+//carousel-p2
+
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
